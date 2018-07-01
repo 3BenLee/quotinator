cdf38ae653344a4da72db16b6d5d413efb6d7037
Ben <bendesigns76@gmail.com>
2018-07-02 02:06:14 +0900

    Initial commit.

---
+++ app.js
@@ -0,0 +1,42 @@
+//This means when the page is ready load this function
+$(document).ready(function(){
+	
+var quote
+var author
+	function getNewQuote() {
+		$.ajax({
+			//You can add all this info in the url also.(same result)
+			url: 'http://api.forismatic.com/api/1.0/',
+			jsonp: 'jsonp',
+			dataType: 'jsonp',
+			data: {
+				method: 'getQuote',
+				lang: 'en',
+				format: 'jsonp'				
+			},
+			success: function(response) {
+				quote = response.quoteText;
+				author = response.quoteAuthor;
+				$('#quote').text(quote);
+				if (author) {
+					$('#author').text('- ' + author + ' -');
+				} else {
+					$('#author').text('-Unknown-');
+				}
+			}
+		});
+	}
+	getNewQuote();
+
+	$('#quote-button').on('click', function() {
+		getNewQuote();
+	});
+	$('#twit').on('click', function(event) {
+		window.open('https://twitter.com/intent/tweet?text=' + encodeURIComponent(quote + ' -' + author + '-'));
+	});
+});
+
+
+
+
+	
